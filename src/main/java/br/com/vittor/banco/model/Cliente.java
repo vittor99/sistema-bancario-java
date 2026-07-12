0e6a0ed76d0c609df9ae5e598f7648816aa72abd
vittor99 <kawevittor367@gmail.com>
2026-07-12 20:06:33 -0300

    Implementada entidade Cliente

--- src/main/java/br/com/vittor/banco/model/Cliente.java
+++ src/main/java/br/com/vittor/banco/model/Cliente.java
@@ -1,4 +1,65 @@
 package br.com.vittor.banco.model;
 
 public class Cliente {
+
+    private Long id;
+    private String nome;
+    private String cpf;
+    private String email;
+    private String telefone;
+
+
+    public Cliente( String nome, String cpf, String email, String telefone) {
+        //this.id = id; id removido pois o cliente nao escolhe o id
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+        this.telefone = telefone;
+    }
+    public Cliente() {
+
+    }
+
+    public Long getId(){
+        return this.id;
+    }
+
+    public String getNome(){
+        return this.nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public String getCpf () {
+        return this.cpf;
+    }
+
+    public String getEmail (){
+        return this.email;
+    }
+
+    public void setEmail (String email){
+        this.email = email;
+    }
+
+    public String getTelefone (){
+        return this.telefone;
+    }
+
+    public void setTelefone (String telefone){
+        this.telefone = telefone;
+
+    }
+
+    @Override
+    public String toString(){ //nao exibe cpf
+        return "Cliente{" + "id= " + this.id +
+                ", nome= " + this.nome + ", email= " + this.email +
+                ", telefone= " + this.telefone + "}";
+    }
+
+
+
 }
