54f712248c2747e259402090a631ce16ed4a9647
vittor99 <kawevittor367@gmail.com>
2026-07-13 22:48:47 -0300

    Implementa o modelo inicial da classe Conta

--- src/main/java/br/com/vittor/banco/model/Conta.java
+++ src/main/java/br/com/vittor/banco/model/Conta.java
@@ -1,4 +1,36 @@
 package br.com.vittor.banco.model;
 
 public class Conta {
+
+    private Long numero;
+    private Cliente titular; // para a conta saber quem e seu dono
+    private double saldo;
+    private boolean ativa;
+
+
+    public Conta(Long numero, Cliente titular){
+        this.numero = numero; //verifica
+        this.titular = titular;
+        this.saldo = 0.0;
+        this.ativa = true;
+    }
+
+    public Long getNumero() {
+        return numero;
+    }
+
+    public Cliente getTitular(){
+        return titular;
+    }
+
+    public double getSaldo(){
+        return saldo;
+    }
+
+    public boolean isAtiva(){
+        return ativa;
+    }
+
+
+
 }
